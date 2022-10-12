dc2fa29595bd848a9371b7d8060596fa54c77953
伊東聖矢 <itoseiya1219@gmail.com>
2022-10-12 10:35:48 +0900

    シーン作成wip

---
+++ Assets/Script/Tile/Sugoroku/TileSugorokuSetting.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TileSugorokuSetting : MonoBehaviour
+{
+	[ContextMenu("AddTileCell")]
+	public void AddTileCell()
+	{
+		var mastGameObject = GetComponentsInChildren<Transform>().Select(t => t.gameObject);
+
+		foreach (Transform child in transform)
+		{
+			if (!child.GetComponent<TileCellSugoroku>())
+			{
+				child.gameObject.AddComponent<TileCellSugoroku>();
+			}
+		}
+	}
+
+	[ContextMenu("Remove")]
+	public void RemoveTileCell()
+	{
+		foreach (Transform child in transform)
+		{
+			if (child.GetComponent<TileCellSugoroku>())
+			{
+				DestroyImmediate(child.gameObject.GetComponent<TileCellSugoroku>());
+			}
+		}
+	}
+
+	[ContextMenu("SetRelationCell")]
+	public void TileCellSetRelations()
+	{
+		var cells = GetComponentsInChildren<TileCellSugoroku>();
+		foreach (var cell in cells)
+		{
+			cell.SetRelation(cells.ToList());
+		}
+	}
+}
---
+++ Assets/Script/Tile/TileSetting.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TileSetting : MonoBehaviour
+{
+	[ContextMenu("AddTileCell")]
+	public void AddTileCell()
+	{
+		var mastGameObject = GetComponentsInChildren<Transform>().Select(t => t.gameObject);
+
+		foreach (Transform child in transform)
+		{
+			if (!child.GetComponent<TileCell>())
+			{
+				child.gameObject.AddComponent<TileCell>();
+			}
+		}
+	}
+
+	[ContextMenu("Remove")]
+	public void RemoveTileCell()
+	{
+		foreach (Transform child in transform)
+		{
+			if (child.GetComponent<TileCell>())
+			{
+				DestroyImmediate(child.gameObject.GetComponent<TileCell>());
+			}
+		}
+	}
+
+	[ContextMenu("SetRelationCell")]
+	public void TileCellSetRelations()
+	{
+		var cells = GetComponentsInChildren<TileCell>();
+		foreach (var cell in cells)
+		{
+			cell.SetRelation(cells.ToList());
+		}
+	}
+
+}
